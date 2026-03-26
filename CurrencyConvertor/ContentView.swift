113adc21960a113ba28c6443cada0fbcb8e22d29
Gia Huy Nguyen <nggiahuy09@gmail.com>
2026-03-26 21:42:57 +0700

    Initial Commit

---
+++ CurrencyConvertor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CurrencyConvertor
+//
+//  Created by Nguyễn Gia Huy on 26/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CurrencyConvertor/CurrencyConvertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConvertorApp.swift
+//  CurrencyConvertor
+//
+//  Created by Nguyễn Gia Huy on 26/3/26.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConvertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
