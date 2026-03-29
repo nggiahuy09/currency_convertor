07b0174429ca0b38876f05cc7f417da9130079ab
Gia Huy Nguyen <nggiahuy09@gmail.com>
2026-03-29 17:50:01 +0700

    feat: implement currency menu in input fields

--- CurrencyConvertor/ViewModels/ContentViewModel.swift
+++ CurrencyConvertor/ViewModels/ContentViewModel.swift
@@ -10,18 +10,24 @@ import Combine
 
 class ContentViewModel: ObservableObject {
 
-    init(convertedAmount: Double = 0.0, baseAmount: Double = 0.0) {
-        self.convertedAmount = convertedAmount
+    init(baseAmount: Double = 0.0, baseCurrency: CurrencyChoice = .Usa, convertedAmount: Double = 0.0, convertedCurrency: CurrencyChoice = .Vietnam) {
         self.baseAmount = baseAmount
+        self.baseCurrency = baseCurrency
+        self.convertedAmount = convertedAmount
+        self.convertedCurrency = convertedCurrency
     }
 
-    @Published var convertedAmount: Double
     @Published var baseAmount: Double
+    @Published var baseCurrency: CurrencyChoice
+    @Published var convertedAmount: Double
+    @Published var convertedCurrency: CurrencyChoice
+
 
     var numberFormatter: NumberFormatter {
         let numberFormatter = NumberFormatter()
         numberFormatter.numberStyle = .currency
         numberFormatter.currencySymbol = ""
+        numberFormatter.maximumFractionDigits = 2
 
         return numberFormatter
     }
--- CurrencyConvertor/Views/ContentView.swift
+++ CurrencyConvertor/Views/ContentView.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct ContentView: View {
 
-    @StateObject private var viewModel = ContentViewModel(convertedAmount: 0.0, baseAmount: 0.0)
+    @StateObject private var viewModel = ContentViewModel(baseAmount: 0.0, baseCurrency: .Usa, convertedAmount: 0.0, convertedCurrency: .Vietnam)
 
     var body: some View {
         VStack(alignment: .leading, spacing: 40.0) {
@@ -21,12 +21,34 @@ struct ContentView: View {
                 TextField("", value: $viewModel.baseAmount, formatter: viewModel.numberFormatter)
                     .keyboardType(.decimalPad)
                     .font(.body)
-                    .padding(.horizontal, 10.0)
-                    .padding(.vertical, 8.0)
+                    .padding(.horizontal, 12.0)
+                    .padding(.vertical, 16.0)
+                    
                     .overlay(content: {
-                        RoundedRectangle(cornerRadius: 10.0)
+                        RoundedRectangle(cornerRadius: 8.0)
                             .fill(.clear)
                             .stroke(.gray, lineWidth: 1.5)
+                    })
+                    .overlay(alignment: .trailing, content: {
+                        Menu(content: {
+                            ForEach(CurrencyChoice.allCases, id: \.id, content: { currencyChoice in Button(action: {
+                                viewModel.baseCurrency = currencyChoice
+                            }, label: { Text(currencyChoice.fetchMenuName()) })
+                            })
+                        }, label: {
+                            HStack(spacing: 4.0) {
+                                viewModel.baseCurrency.image()
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(maxWidth: 30.0, maxHeight: 30.0)
+                                    .clipShape(Circle())
+                                Text(viewModel.baseCurrency.rawValue)
+                                    .font(.caption)
+                                    .fontWeight(.bold)
+                                Image(systemName: "chevron.down")
+                                    .font(.caption)
+                            }.foregroundStyle(.black)
+                        }).padding(.trailing, 8.0)
                     })
             }
 
@@ -37,7 +59,7 @@ struct ContentView: View {
                     .foregroundStyle(.green)
                 Spacer()
             }
-            
+
             VStack(alignment: .leading, spacing: 12.0) {
                 Text("Converted To")
                     .font(.body)
@@ -45,18 +67,40 @@ struct ContentView: View {
                 TextField("", value: $viewModel.convertedAmount, formatter: viewModel.numberFormatter)
                     .keyboardType(.decimalPad)
                     .font(.body)
-                    .padding(.horizontal, 10.0)
-                    .padding(.vertical, 8.0)
+                    .padding(.horizontal, 12.0)
+                    .padding(.vertical, 16.0)
                     .overlay(content: {
-                        RoundedRectangle(cornerRadius: 10.0)
+                        RoundedRectangle(cornerRadius: 8.0)
                             .fill(.clear)
                             .stroke(.gray, lineWidth: 1.5)
+                    })
+                    .overlay(alignment: .trailing, content: {
+                        Menu(content: {
+                            ForEach(CurrencyChoice.allCases, id: \.id, content: { currencyChoice in Button(action: {
+                                viewModel.convertedCurrency = currencyChoice
+                            }, label: { Text(currencyChoice.fetchMenuName()) })
+                            })
+                        }, label: {
+                            HStack(spacing: 4.0) {
+                                viewModel.convertedCurrency.image()
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(maxWidth: 30.0, maxHeight: 30.0)
+                                    .clipShape(Circle())
+                                Text(viewModel.convertedCurrency.rawValue)
+                                    .font(.caption)
+                                    .fontWeight(.bold)
+                                Image(systemName: "chevron.down")
+                                    .font(.caption)
+                            }.foregroundStyle(.black)
+
+                        }).padding(.trailing, 8.0)
                     })
             }
 
             HStack {
                 Spacer()
-                Text("1.00000 USD = 10000 VND")
+                Text("\(viewModel.baseAmount) \(viewModel.baseCurrency.rawValue) = \(viewModel.convertedAmount) \(viewModel.convertedCurrency.rawValue)")
                     .font(.subheadline)
                     .fontWeight(.bold)
                 Spacer()
